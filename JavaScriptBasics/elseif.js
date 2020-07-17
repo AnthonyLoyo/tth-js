b16444a0b953ec972452acbc835325c30deb2c28
Anthony Loyo <a.loyo88@gmail.com>
2020-07-17 13:58:23 -0700

    worked on elseif and started quiz

---
+++ JavaScriptBasics/elseif.js
@@ -0,0 +1,20 @@
+// a ELSEIF clause is used to provide multiple program paths with numerous conditions.
+
+// Exercise:
+// in this exercise we create a weather variable and give a value of snow.
+// next we create a if statement with multiple conditions to be tested instead of just 2, we do this by using a else if clause. this gives the ability to test mulitple conditions in a if statement. 
+// in this if statement we want to do a activity based on the weather. 
+// our variable is then ran through our if statment and tested against mulitple conditions using the else if clause.
+// the weather variable is then tested against the  if, else if and else statements conditionals. once one of the conditionals in the if, else if or else statements is strictly matched to the value inside weather it excutes that code block and logs the message into console. 
+
+let weather = "sun";
+
+if ( weather === "sun" ) {
+    console.log("lets go swimming!")
+} else if ( weather === "rain") {
+    console.log("lets stay inside and read a book")
+} else if ( weather === "snow") {
+    console.log("lets go sledding!")
+} else {
+    console.log("its gonna be a lazy day")
+}
---
+++ JavaScriptBasics/quiz.js
@@ -0,0 +1,33 @@
+// 1) Store correct answers
+// - When quiz begins no answers are correct
+
+// 2) Store the rank of a player
+
+// 3) Select the <main> HTML element
+
+// 4) Ask at least 5 questions
+// - Store each answer in a variable
+// - Keep track of the number of correct answers
+
+// 5) Rank player based on number of correct answer
+// - 5 correct = Gold
+// - 3-4 correct = Silver
+// - 1-2 correct = Bronze
+// - 0 correct = no crown
+
+// 6) Output results to the <main> element
+
+
+let correctAnswer = 0;
+
+let playerRank = "";
+
+let questionOne = prompt("What does HTML stand for?");
+
+let questionTwo = prompt("What does CSS stand for?");
+
+let questionThree = prompt("What is Bootstrap?");
+
+let questionFour = prompt("Are Java and JavaScript the same?");
+
+let questionFive = prompt("Who created JavaScript?");
