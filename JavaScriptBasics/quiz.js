6f38c8d948d6b2c8c7d5eae70980ff2e8b7c482c
Anthony Loyo <a.loyo88@gmail.com>
2020-07-22 13:42:45 -0700

    Loyo quiz complete, Guil Quiz Solution explained

--- JavaScriptBasics/quiz.js
+++ JavaScriptBasics/quiz.js
@@ -17,7 +17,9 @@
 
 // 6) Output results to the <main> element
 
-// this is my solution for the quiz, it is completed 4/5
+
+// Loyo's Solution:
+
 let correctAnswer = 0;
 
 let playerRank = " ";
@@ -27,7 +29,7 @@ let questionOne = prompt("What is short for Hyper Text Markup Language?");
 
     if( questionOne.toUpperCase() === "HTML" ) {
         correctAnswer += 1;
-    }
+    } 
 
 let questionTwo = prompt("What is short for Cascading Styel Sheets?");
 
@@ -35,9 +37,9 @@ let questionTwo = prompt("What is short for Cascading Styel Sheets?");
         correctAnswer += 1;
     }
 
-let questionThree = prompt("What is Bootstrap?");
+let questionThree = prompt("Bootstrap is a?");
 
-    if( questionThree.toUpperCase() === "A FRAMEWORK" ) {
+    if( questionThree.toUpperCase() === "FRAMEWORK" ) {
         correctAnswer += 1;
     }
 
@@ -53,8 +55,71 @@ let questionFive = prompt("Who created JavaScript?");
         correctAnswer += 1;
     }
 
+if(correctAnswer === 1){
+    playerRank += "Bronze";
+} else if( correctAnswer === 2){
+    playerRank += "Bronze";
+} else if( correctAnswer === 3){
+    playerRank += "Silver";
+} else if( correctAnswer === 4){
+    playerRank += "Silver";
+} else if( correctAnswer === 5){
+    playerRank += "Gold";
+} else {
+    playerRank += "No Crown";
+}
+
+let finalScore = `<h2>Number of correct answer ${correctAnswer}</h2>`;
+document.querySelector("h2").innerHTML = finalScore;
+
+let finalRank = `<p> Final rank is ${playerRank}</p>`;
+document.querySelector("p").innerHTML = finalRank;
 
 
-let currentScore = `<h2>Number of correct answer ${correctAnswer}</h2>`;
+// Guils Solution:
 
-document.querySelector("main").innerHTML = currentScore;
+let correct = 0;
+
+let rank;
+
+const main = document.querySelector('main');
+
+const answer1 = prompt("Name a programming language thats also a gem?");
+if(answer1.toUpperCase() === "RUBY"){
+    correct += 1;
+}
+
+const answer2 = prompt("Name a programming language thats also a snake?");
+if(answer2.toUpperCase() === "PYTHON"){
+    correct += 1;
+}
+
+const answer3 = prompt("What language do you use to style web pages?");
+if(answer3.toUpperCase() === "CSS"){
+    correct += 1;
+}
+
+const answer4 = prompt("What language do you use to build the structure of web pages?");
+if(answer4.toUpperCase() === "HTML"){
+    correct += 1;
+}
+
+const answer5 = prompt("What Language do you use to add interactivity to a web page?");
+if(answer5.toUpperCase() === "JavaScript"){
+    correct += 1;
+}
+
+if(correct === 5){
+    rank =  "Gold";
+} else if(correct >= 3){
+    rank = "Silver";
+} else if (correct >= 2){
+    Rank = "Bronze";
+} else {
+    rank = "No Crown";
+}
+
+main.innerHTML = `
+    <h2> you got ${correct} out of 5 questions correct. </h2>
+    <p>Crown earnder: ${rank}</p>
+`;
