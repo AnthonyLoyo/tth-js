c7ab8bd29b939e71d72cc382ce9683b2417f2aca
Anthony Loyo <a.loyo88@gmail.com>
2020-07-23 14:16:03 -0700

    started JavaScriipt Numbers

---
+++ JavaScriptNumbers/main.js
@@ -0,0 +1,69 @@
+// Example of whole numbers(Intergers):
+//05
+//0
+//-100
+//9999
+
+
+// Exapmle of fraction whole numbers(decimal points):
+//3.14
+//-9.8888
+//.0000009
+
+
+// Example of scientific notation:
+//9e-6 // same as .000009
+//9e+6 // same as 9000000
+
+
+// Example of the additional operator(+):
+//2 + 7 // 9
+
+
+// Example of the minus operator(-):
+//4 - 3 // 1
+
+
+// Example of the division operator(/):
+//6 / 3 // 2
+
+
+// Example of the multiplication operator(*):
+//10 * 9 // 90
+
+
+// create a variable named score
+//var score;
+// and store the number 0 in it
+//let score = 0;
+// here we take the variable score and assign it to add whatever
+// amount is in score currently plus 100;
+//score = score + 100;
+
+
+// Assignment Operators
+// there are shorthand methods for performing each basic mathamatical equation
+
+//score = score + 10; // score += 10;
+//score = score - 20; // score -= 20;
+//score = score * 5;  // score *= 5;
+//score = score / 2;  // score /+ 2;
+
+
+const secsPermin = 60;
+const minPerHour = 60;
+const hourPerDay = 24;
+const daysPerWeek = 7;
+const weeksPerYear = 52;
+
+const secondsPerDay = secsPermin * minPerHour * hourPerDay;
+
+console.log(`There are ${secondsPerDay} seconds in a day.`);
+
+// challenge
+const yearsAlive = 30;
+
+const secondsAlive = secsPermin * 
+
+
+console.log(`Ive been alive for more than X seconds`)
